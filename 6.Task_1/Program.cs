e991d2169350038091bec38db678d3bef711959f
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-09-12 17:31:04 +0900

    Add files via upload

---
+++ 6.Task_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player playerOne = new Player();
+            Player playerTwo = new Player(200,200,200,2);
+            playerOne.ShowPlayerInfo();
+            Console.WriteLine();
+            playerTwo.ShowPlayerInfo();
+            Console.ReadKey();
+        }
+    }
+
+    class Player
+    {
+        public int Health;
+        public int Mana;
+        public int Stamina;
+        public int Level;
+
+        public Player()
+        {
+            Health = 100;
+            Mana = 40;
+            Stamina = 20;
+            Level = 1;
+        }
+
+        public Player(int healt, int mana, int stamina, int level)
+        {
+            Health = healt;
+            Mana = mana;
+            Stamina = stamina;
+            Level = level;
+        }
+
+        public void ShowPlayerInfo()
+        {
+            Console.WriteLine("Health = " + Health);
+            Console.WriteLine("Mana = " + Mana);
+            Console.WriteLine("Stamina = " + Stamina);
+            Console.WriteLine("Level = " + Level);
+        }
+    }
+}
